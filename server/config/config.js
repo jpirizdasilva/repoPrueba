0dc71d362e15a0429757baa08d12509415629a66
Jeremias <jeremias.piriz.da.silva@gmail.com>
2020-01-10 16:21:27 -0300

    agregamos variable de entorno para mongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -17,11 +17,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //   Base de datos:
 //===============================================
 
-// let urlDB
-// if(process.env.NODE_ENV==='dev'){
-//     urlDB = 'mongodb://localhost:27017/rapimercadoDB'
-// }else{
-    urlDB = 'mongodb+srv://adminrapimercado:algoRELOCO2020@rapimercado-118c2.mongodb.net/rapimercadoDB'
-// }
+let urlDB
+if(process.env.NODE_ENV==='dev'){
+    urlDB = 'mongodb://localhost:27017/rapimercadoDB'
+}else{
+    urlDB = process.env.MONGO_URI
+}
 
 process.env.URL_DB = urlDB
